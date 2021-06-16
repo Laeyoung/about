2f5bed81b5d67c2f398e477253b19c13118c1ec6
Laeyoung Chang <laeyoung@comcom.ai>
2021-06-16 23:36:21 +0900

    feat: update typeform style radio button

--- src/stories/TypeformRadio.tsx
+++ src/stories/TypeformRadio.tsx
@@ -2,25 +2,8 @@ import React from 'react';
 import './typeformradio.css';
 
 export interface TypeformRadioProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
+  items: string[];
+  
   onClick?: () => void;
 }
 
@@ -28,40 +11,23 @@ export interface TypeformRadioProps {
  * Primary UI component for user interaction
  */
 export const TypeformRadio: React.FC<TypeformRadioProps> = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
+  items = ['In this tutorial, we will cover how to create a simple AngularJS project with three controllers.', 'I\'m sorry for how quickly we all arrived in here.'],
   ...props
 }) => {
-  const mode = primary ? 'storybook-typeformradio--primary' : 'storybook-typeformradio--secondary';
   return (
-    // <button
-    //   type="button"
-    //   className={['storybook-typeformradio', `storybook-typeformradio--${size}`, mode].join(' ')}
-    //   style={{ backgroundColor }}
-    //   {...props}
-    // >
-    //   {label}
-    // </button>
     <form>
-      <div className="radiobtn">
-        <input type="radio" id="huey"
-              name="drone" value="huey" checked />
-        <label htmlFor="huey">we're ready to get on with writing our first</label>
-      </div>
-    
-      <div className="radiobtn">
-        <input type="radio" id="dewey"
-              name="drone" value="dewey" />
-        <label htmlFor="dewey">In this tutorial, we will cover how to create a simple AngularJS project with three controllers</label>
-      </div>
-    
-      <div className="radiobtn">
-        <input type="radio" id="louie"
-              name="drone" value="louie" />
-        <label htmlFor="louie">I'm sorry for how quickly we all arrived in here.</label>
-      </div>
+      {
+        items.map((item, index) => {
+          const id = `${index}`
+
+          return (
+            <div className="radiobtn">
+              <input type="radio" id={id} name='typeform-radio' value={index} />
+              <label htmlFor={id}>{item}</label>
+            </div>
+          )
+        })
+      }
     </form>
   );
 };
