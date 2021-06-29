ec19267a4d7959c623252fe01e315999c53981e8
Laeyoung Chang <laeyoung@comcom.ai>
2021-06-29 21:08:27 +0900

    feat: add top header using material app-bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import stringHash from 'string-hash';
 import produce from 'immer';
 import _ from 'lodash';
@@ -17,11 +22,10 @@ const NextQuestionQueue = [
   '커먼컴퓨터를 어떻게 시작하게 되셨나요?',
   '커먼컴퓨터를 시작하기 전에는 어떤 일을 하셨나요?',
   'AI Writer는 어떻게 시작된 서비스인가요?',
-  '커먼컴퓨터에서 서비스를 만들면서 겪는 가장 큰 어려움은 뭔가요?',
+  '서비스를 만들면서 겪는 가장 큰 어려움은 뭔가요?',
   '커먼컴퓨터의 앞으로 목표는 뭔가요?',
   '커먼컴퓨터의 비젼은 뭔가요?',
-  '커먼컴퓨터의 비젼을 달성하기 위해 커먼컴퓨터가 겪는 가장 큰 챌린지는 뭔가요?',
-  '한국에서 인공지능과 블록체인 스타트업을 하는 환경은 어떤가요?',
+  '커먼컴퓨터가 겪는 가장 큰 챌린지는 뭔가요?',
   '커먼컴퓨터의 팀문화는 어떠한가요?',
   '커먼컴퓨터가 현재 찾고 있는 팀원이 있나요?',
 
@@ -32,6 +36,20 @@ const NextQuestionQueue = [
   // 'aFan은 다양한',
   // '2021년의 커먼컴퓨터의 목표는',
 ];
+const FIRST_QUESTION = NextQuestionQueue.shift() as string;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  offset: theme.mixins.toolbar,
+}));
 
 function QuestionAnswerList(
   questions: string[],
@@ -49,15 +67,15 @@ function QuestionAnswerList(
 }
 
 const IndexPage: NextPage = () => {
-  const [questions, setQuestions] = useState([
-    NextQuestionQueue.shift() ?? 'Test question?',
-  ]);
+  const [questions, setQuestions] = useState([FIRST_QUESTION]);
   const [answers, setAnswers] = useState([] as Answer[]);
   const onSelectCallback = useCallback(
     (answer: Answer) => {
       setQuestions(
         produce(questions, (draft) => {
           const nextQuestion = NextQuestionQueue.shift();
+          console.log(nextQuestion);
+          console.log(questions.length);
           if (nextQuestion) draft.push(nextQuestion);
         }),
       );
@@ -70,58 +88,27 @@ const IndexPage: NextPage = () => {
     [questions],
   );
 
+  const classes = useStyles();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>About ComCom</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <header className={styles.header}>
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */}
+      <AppBar position="fixed" style={{ background: '#764fcd' }}>
+        <Toolbar>
+          <Typography variant="h5" className={classes.title}>
+            커먼컴퓨터에 관하여
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <div className={styles.body}>
-        <h1>커먼컴퓨터에 관하여</h1>
+        <div className={styles.description}>
+          커먼컴퓨터에 대해 학습한 AI와의 10문 10답입니다.
+          <br />
+          여러분이 생각하시는 커먼컴퓨터는 어떤 모습에 가깝나요? 🤔
+        </div>
         {QuestionAnswerList(questions, onSelectCallback)}
       </div>
     </div>
